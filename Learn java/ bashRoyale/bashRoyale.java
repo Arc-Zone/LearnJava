7f08d3e8011ef96df0ef88d249b22a456e035090
Zone <enzo.gomez@3wa.io>
2025-07-29 10:30:50 +0200

    add bashRoyale project

---
+++ Learn java/ bashRoyale/bashRoyale.java
@@ -0,0 +1,81 @@
+
+import java.util.Scanner;
+public class bashRoyale{
+        private static final Scanner sc = new Scanner(System.in);
+
+
+    public static void menu() {
+        System.out.println("______Bash Royale______");
+        System.out.println("1. Play");
+        System.out.println("2. Option");
+        System.out.println("3. Exit");
+        System.out.print("Choix : ");
+    }
+            
+    public static void Delay(int sec){
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Le délai a été interrompu !");
+        }
+       
+    }
+
+    public static void game() {
+        boolean gaming = true;
+        while(gaming){
+            Delay(2);
+            System.out.println("Initialisation du shell...");
+            Delay(2);
+            System.out.println("Chargement des variables de chaos...");
+            Delay(2);
+            System.out.println("Connexion au Serveur Principal : [Bash Royale Arena]...");
+            Delay(2);
+            System.out.println("Analyse des joueurs connectés... 64 terminaux détectés.");
+            Delay(2);
+            System.out.println("But du jeu : Survivre. Dominer. Commander.");
+            // Simulation d’une boucle de jeu ou action
+            System.out.println("\n========== [ SÉLECTION DU COMBATTANT ] ==========");
+            chooseCaracter();
+
+            String input = sc.nextLine().trim().toLowerCase();
+                if (input.equals(5)) {
+                        gaming = false;
+                        Delay(1);
+                }
+        }
+    }
+        public static void chooseCaracter(){
+            System.out.println("1- Guerrier");
+            System.out.println("2 -Mage");
+            System.out.println("3 -Rôdeur");
+            System.out.println("4 -Soigneur");
+            System.out.println("5 -Exit");
+            System.out.print("Choix : ");
+        }
+
+    public static void main(String[] args) {
+    int choice = 0 ;
+    do{
+        menu();
+        choice = Integer.parseInt(sc.nextLine());
+        
+        switch (choice) {
+            case 1:
+                game();
+                break;
+                case 2:
+                System.out.println("Pas encore implémenté, feignant !");
+                break;
+            case 3:
+                System.out.println("Fermeture du shell. À bientôt, commandant.");
+                break;
+
+            default:
+                throw new AssertionError();
+        }
+    } while(choice != 3);
+
+    }
+}
